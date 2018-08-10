5d0110e39c53dc6af961d5fe107d1b82a84cc7e9
Vusal Gakhramanov <nuruva02@gmail.com>
2018-08-10 18:57:32 +0300

    Update 2018-08-10T15:57:27.046Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,16 @@
 var navburlink = document.querySelector('.nav__burger-link');
 var navadapt = document.querySelector('.navadapt');
-var navburlinkactive = document.querySelector('.nav__burger-link--active');
-var navadaptlink = document.querySelector('.navadapt__link');
-
 navburlink.addEventListener('click', function() {
-    navburlink.classList.add("nav__burger-link--active");
-    navadapt.style.display = 'flex';
-})
-//navadaptlinkactive.addEventListener('click', function() {
-//    navburlink.classList.remove("nav__burger-link--active");
-//    navadapt.style.display = 'none';
-//})
-navadaptlink.addEventListener('click', function() {
-    navburlink.classList.remove("nav__burger-link--active");
-    navadapt.style.display = 'none';
-})
+    navburlink.classList.toggle("nav__burger-link--active");
+    navadapt.classList.toggle("navadapt_active");
+});
 
-//if (navburlink.classList === "nav__burger-link--active") {
-//    navburlink.classList.remove("nav__burger-link--active");
-//    navadapt.style.display = 'flex';
-//}else{navburlink.classList += "nav__burger-link--active";
-//navadapt.style.display = 'flex';
-//    }
-//})
 
-/*function myfunction () {
-    var x = document.getElementsByClassName("navadapt-link");
-    if (x.className === "nav__burger-link--active") {
-        x.className -= " nav__burger-link--active";
-    }else{x.className += " nav__burger-link--active";
-    }
-}*/
+
+var navadaptlink = document.querySelectorAll ("a.navadapt__link");
+for (var i=0; i<navadaptlink.length; i++){
+	navadaptlink[i].onclick = function() {
+        navburlink.classList.toggle("nav__burger-link--active");
+        navadapt.classList.toggle("navadapt_active");
+    };
+};
