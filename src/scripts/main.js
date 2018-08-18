6977150f013c4008770ba39f872675c667f496b6
Vusal Gakhramanov <nuruva02@gmail.com>
2018-08-18 17:59:04 +0300

    js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -53,6 +53,13 @@ function accordionMenu() {
         let menuit = target.parentNode;
         let content = target.nextElementSibling;
 
+        const tarWidth = target.clientWidth;
+        const windowWidth = document.documentElement.clientWidth;
+        const layoutContentWidth = 480;
+        const breakpointPhone = 480;
+        const closeMenuWidth = tarWidth * menus.length;
+        const openMenuWidth = closeMenuWidth + layoutContentWidth;
+
         if (event.target.classList.contains("menu__title")) {
             name()
         }
@@ -68,13 +75,34 @@ function accordionMenu() {
             for (const iterator of menus) {
                 if (iterator !== menuit) {
                     iterator.classList.remove("menu__item--active");
+                    iterator.lastElementChild.style.width = 0;
+                    menuAccord.style.transform = 'translateX(0)';
                 }
             }
 
             if (menuit.classList.contains("menu__item--active")) {
                 menuit.classList.remove("menu__item--active");
+                content.style.width = 0;
             } else {
                 menuit.classList.add("menu__item--active");
+                
+                if (windowWidth > breakpointPhone && windowWidth < openMenuWidth) {
+                    content.style.width = windowWidth - closeMenuWidth + 'px';
+                } else if (windowWidth <= breakpointPhone) {
+                    let num
+
+                    for (let i = 0; i < menus.length; i++) {
+
+                        if(menus[i] === menuit) {
+                            num = menus.length - (i + 1)
+                        }
+                    }
+
+                    menuAccord.style.transform = 'translateX(${tarWidth * num}px)';
+                    content.style.width = windowWidth - tarWidth + 'px';
+                } else {
+                    content.style.width = 480 + 'px';
+                }  
             }
         }
     });
@@ -108,17 +136,26 @@ left.addEventListener("click", function() {
     }
 });
 
-
-
-const openButton = document.querySelectorAll("button.button--comments");
-const successOverlay = openOverlay("Мысли все о них и о них, о них и о них. Нельзя устоять, невозможно забыть.Никогда не думал что булочки могут быть такими мягкими,котлета такой сочной,а сыр таким расплавленным.Мысли все о них и о них, о них и о них. Нельзя устоять, невозможно забыть.Никогда не думал что булочки могут быть такими мягкими,котлета такой сочной,а сыр таким расплавленным.");
-
-for (var button of openButton) {
-	button.addEventListener('click', function() {
-        document.body.appendChild(successOverlay);
-    });
+////////
+function openReviews() {
+    const openButton = document.querySelectorAll(".button--comments");
+    for (var button of openButton) {
+        button.addEventListener('click', function(e) {
+            let content = e.target.previousElementSibling.textContent;
+            let title = e.target.previousElementSibling.previousElementSibling.textContent;
+            if (e.target.classList.contains("button--desctop")) {
+                document.body.appendChild(openOverlay(content, title));
+            } else {
+                content = e.target.previousElementSibling.previousElementSibling.textContent;
+                title = e.target.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+                document.body.appendChild(openOverlay(content, title));
+            }
+        });
+    }
 }
-function openOverlay(content) {
+openReviews()
+//////
+function openOverlay(content, title) {
   const overlayElement = document.createElement("div");
   overlayElement.classList.add("overlay");
   overlayElement.addEventListener("click", e => {
@@ -136,7 +173,7 @@ function openOverlay(content) {
 
   const titleElement = document.createElement("h3");
   titleElement.classList.add("comments__overlaytitle");
-  titleElement.textContent = "константин спилберг";
+  titleElement.textContent = title;
 
   const closeElement = document.createElement("a");
   closeElement.classList.add("close");
@@ -158,82 +195,82 @@ function openOverlay(content) {
 
 const myForm = document.querySelector('.form');
 const send = document.querySelector('.send');
- send.addEventListener('click', event => {
-     event.preventDefault();
-
-     if (validateForm(myForm)) {
-
-
-const openButton = document.querySelector(".send");
-const successOverlay = openOverlay("Сообщение отправлено");
-
-openButton.addEventListener('click', function() {
-    document.body.appendChild(successOverlay);
-});
-
-function openOverlay(content) {
-  const overlayElement = document.createElement("div");
-  overlayElement.classList.add("over-lay");
-  overlayElement.addEventListener("click", e => {
-    if (e.target === overlayElement) {
-    closeElement.click();
-    }
-  });
-
-  const containerElement = document.createElement("div");
-  containerElement.classList.add("overlay-container");
-
-  const contentElement = document.createElement("div");
-  contentElement.classList.add("overlay-content");
-  contentElement.innerHTML = content;
-
-  const closeElement = document.createElement("button");
-  closeElement.classList.add("button_close");
-  closeElement.textContent = "закрыть";
-  closeElement.addEventListener("click", function() {
-    document.body.removeChild(overlayElement);
-  });
-
-  overlayElement.appendChild(containerElement);
-  containerElement.appendChild(contentElement);
-  containerElement.appendChild(closeElement);
-
-  return overlayElement;
-}
-
-
+send.addEventListener('click', event => {
+    event.preventDefault();
+    
+    if (validateForm(myForm)) {
         const data = {
             name: myForm.elements.name.value,
             phone: myForm.elements.phone.value,
-            comment: myForm.elements.comment.value
+            comment: myForm.elements.comment.value,
+            to: "mail@mail.ru"
         };
+        console.log(data);
         
         const xhr = new XMLHttpRequest();
         xhr.responseType = 'json';
-        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail/fail');
+        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+        console.log(JSON.stringify(data));
+        
         xhr.send(JSON.stringify(data));
         xhr.addEventListener('load', () => {
+            console.log(xhr.response);
+            
             if (xhr.response.status !== 0) {
-                console.log('все ок');
+                console.log(xhr.response);
+                document.body.appendChild(openOverlayForm(xhr.response));
+            } else {
+                console.log(xhr.response.message);
+                document.body.appendChild(openOverlayForm(xhr.response.message));
             }
         });
+
     }
- });
+});
+function openOverlayForm(content) {
+    const overlayElement = document.createElement("div");
+    overlayElement.classList.add("over-lay");
+    overlayElement.addEventListener("click", e => {
+        if (e.target === overlayElement) {
+        closeElement.click();
+        }
+    });
 
- function validateForm(form) {
-     let valid = true;
+    const containerElement = document.createElement("div");
+    containerElement.classList.add("overlay-container");
 
-     if (!validateformblock(form.elements.name)) {
-         valid = false;
-     }
-     if (!validateformblock(form.elements.phone)) {
+    const contentElement = document.createElement("div");
+    contentElement.classList.add("overlay-content");
+    contentElement.innerHTML = content;
+
+    const closeElement = document.createElement("button");
+    closeElement.classList.add("button_close");
+    closeElement.textContent = "закрыть";
+    closeElement.addEventListener("click", function() {
+        document.body.removeChild(overlayElement);
+    });
+
+    overlayElement.appendChild(containerElement);
+    containerElement.appendChild(contentElement);
+    containerElement.appendChild(closeElement);
+
+    return overlayElement;
+}
+
+function validateForm(form) {
+    let valid = true;
+
+    if (!validateformblock(form.elements.name)) {
         valid = false;
-     }
-     if (!validateformblock(form.elements.comment)) {
-        valid = false;
+    }
+    if (!validateformblock(form.elements.phone)) {
+    valid = false;
+    }
+    if (!validateformblock(form.elements.comment)) {
+    valid = false;
     }
     return valid;
- }
+}
 
  function validateformblock(formblock) {
         formblock.nextElementSibling.textContent = formblock.validationMessage;
